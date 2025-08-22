2599df9dd12c0a4996ad1673381007a882736145
Boothu <declanryan.db@gmail.com>
2025-08-22 15:26:44 +0100

    Add search input and button and add clear comments

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+// Allows for storing and updating state in a component
 import { useState } from "react";
 
+// App component is what returns the UI
 function App() {
+  // useState lets this component have state (memory)
+  // Make a state variable 'query' and a setter 'setQuery'
+  // Calling 'setQuery(...)' updates 'query' and tells React to re-render the UI
   const [query, setQuery] = useState("");
   return (
-    <main className="min-h-screen flex items-center justify-center bg-gray-50">
-      <div className="p-6 rounded-lg shadow bg-white">
-        <h1 className="text-2xl font-semibold">Speedrun Dashboard</h1>
-        <p className="text-gray-600">Tailwind is working</p>
+    <main>
+      <h1>Speedrun Records Dashboard</h1>
+
+      <div>
+        <input
+          placeholder="Search for a game"
+          value={query}
+          // Update 'query' with entered text
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            const trimmed = query.trim();
+            // If nothing is entered return early
+            if (!trimmed) return;
+            // Placeholder for now
+            console.log("Searching for:", trimmed);
+          }}
+        >
+          Search
+        </button>
       </div>
     </main>
   );
